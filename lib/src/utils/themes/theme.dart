45097a950749608d83f3beaa70c63a7ef4303b0b
Zainab Asif <asifzaynab4321@gmail.com>
2025-07-31 01:40:30 +0500

    Saves Changes

--- lib/src/utils/themes/theme.dart
+++ lib/src/utils/themes/theme.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:login/src/utils/themes/widget%20theme/text_theme.dart';
 
 class AppTheme {
+
+  //we dont want to create instance of this class,so we make the constructor private
+  AppTheme._();
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    primarySwatch: Colors.yellow,
+    textTheme: textTheme.lightTheme.textTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.pink, // Button color
+      foregroundColor: Colors.white, // Text color
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8), // Button shape
+      ),
+    ) ),
+    primarySwatch: Colors.pink, 
     scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.pink,
+      foregroundColor: Colors.white,
+    ),  
   );
 
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
+    textTheme: textTheme.darkTheme.textTheme,
     primarySwatch: Colors.deepPurple,
     scaffoldBackgroundColor: Colors.black,
   );
